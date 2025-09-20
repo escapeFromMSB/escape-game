b285557f9866110968cd2ed661a549d1236bed10
Krystal Davison <kdaviso5@kent.edu>
2025-09-20 18:21:14 -0400

    creating map with code
    creating map with code

---
+++ Assets/buildScene.cs
@@ -0,0 +1,222 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buildScene : MonoBehaviour
+{
+    public enum SideOne { left, right, front, back, above, below }
+    public enum SideTwo { left, right, front, back, above, below }
+    private static bool _built = false;
+
+    void Start()
+    {
+        if (_built) return;
+        _built = true;
+
+        float sideOneOffset = 0f;
+        float sideTwoOffset = 0f;
+
+        //--- create the  first floor (plane) ---
+        // createPrimitive creates a 3D object 
+        GameObject firstFloor = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        firstFloor.name = "firstFloor";
+
+        // set the position
+        // vector3 means 3 coordinates
+        firstFloor.transform.position = new Vector3 (0,0,0);
+
+        // set scale to normal scale 
+        // f means float. needs to be float and not double due to performance because float is faster
+        // *** in unity, 1 unit = 1 meter. the scale (1,1,1) is 10x10 by default (for a plane it isx and z axis). ***
+        firstFloor.transform.localScale = new Vector3 (5f,1f,5f); //plane is 50x50
+
+        // --- create wallOne (cube) ---
+        GameObject wallOne = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        wallOne.name = "wallOne";
+        wallOne.transform.localScale = new Vector3 (1f,3f,6f);
+
+        // place wallOne on top of firstFloor 
+        // calculate object positions from the plane’s current size and center, instead of hard-coding numbers.
+        // this way we can update the plane size if needed without messing everyting else up
+        PlaceObjectOnPlane(firstFloor, wallOne, SideOne.left, SideTwo.back, 0f, 0f);
+
+        // a renderer is a unity component that draws the object on the screen
+        // get the renderer components for each object
+        
+        // --- create wallTwo (cube) ---
+        GameObject wallTwo = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        wallTwo.name = "wallTwo";
+        wallTwo.transform.localScale = new Vector3 (2f,3f,1f);
+        PlaceObject(wallOne, wallTwo, SideOne.left, SideTwo.front, 0f, 0f);
+
+        GameObject wallThree = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        wallThree.name = "wallThree";
+        wallThree.transform.localScale = new Vector3 (2f,3f,1f);
+        PlaceObject(wallOne, wallThree, SideOne.right, SideTwo.front, 30f, 3f);
+
+        // Make the cube a color
+        var wr = wallOne.GetComponent<Renderer>();
+        wr.sharedMaterial = new Material(Shader.Find("Standard"));
+        wr.sharedMaterial.color = new Color(0.5f, 0.5f, 1f);
+
+        // --- Aim the Main Camera so you can see everything ---
+        var cam = Camera.main;
+        if (cam != null)
+        {
+            cam.transform.position = new Vector3(0f, 25f, -35f);
+            cam.transform.LookAt(new Vector3(0f, 0f, 0f));
+        }
+        else
+        {
+            Debug.LogWarning("No Main Camera found. Create one (GameObject > Camera).");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+// --- ALIGNMENT FUNCTIONS ---
+
+void PlaceObject(GameObject a, GameObject b, SideOne sideOne, SideTwo sideTwo,
+                 float edgeOneOffset, float edgeTwoOffset)
+{
+    // renderers for both objects
+    Renderer anchorRenderer = a.GetComponent<Renderer>();
+    Renderer objectRenderer = b.GetComponent<Renderer>();
+    // use renderer to get the objects bounds and half of its size 
+    // components of object being aligned to 
+
+    // half-sizes of objects
+    Vector3 halfA = anchorRenderer.bounds.extents;   // anchor half-size
+    Vector3 halfB = objectRenderer.bounds.extents;   // object half-size
+
+    // position of object that is being aligned to (anchor)
+    Vector3 posA = a.transform.position;
+    // temp position 
+    Vector3 pos = posA;
+
+    // top/bottom of anchor
+    float topA = anchorRenderer.bounds.max.y;
+    float bottomA = anchorRenderer.bounds.min.y;
+
+    // --- sideOne ---
+    switch (sideOne)
+    {
+        case SideOne.right: pos.x = posA.x + (halfA.x - halfB.x) + edgeOneOffset; break;
+        case SideOne.left:  pos.x = posA.x - (halfA.x - halfB.x) - edgeOneOffset; break;
+        case SideOne.front: pos.z = posA.z + (halfA.z - halfB.z) + edgeOneOffset; break;
+        case SideOne.back:  pos.z = posA.z - (halfA.z - halfB.z) - edgeOneOffset; break;
+        case SideOne.above: pos.y = topA + halfB.y + edgeOneOffset; break;
+        case SideOne.below: pos.y = bottomA - halfB.y - edgeOneOffset; break;
+    }
+
+    // --- sideTwo ---
+    switch (sideTwo)
+    {
+        case SideTwo.right: pos.x = posA.x + (halfA.x - halfB.x) + edgeTwoOffset; break;
+        case SideTwo.left:  pos.x = posA.x - (halfA.x - halfB.x) - edgeTwoOffset; break;
+        case SideTwo.front: pos.z = posA.z + (halfA.z - halfB.z) + edgeTwoOffset; break;
+        case SideTwo.back:  pos.z = posA.z - (halfA.z - halfB.z) - edgeTwoOffset; break;
+        case SideTwo.above: pos.y = topA + halfB.y + edgeTwoOffset; break;
+        case SideTwo.below: pos.y = bottomA - halfB.y - edgeTwoOffset; break;
+    }
+
+    // move b into place
+    b.transform.position = pos;
+}
+
+void PlaceObjectOnPlane(GameObject a, GameObject b, SideOne sideOne, SideTwo sideTwo,
+                        float edgeOneOffset, float edgeTwoOffset)
+{
+    // renderers for both objects
+    Renderer anchorRenderer = a.GetComponent<Renderer>();
+    Renderer objectRenderer = b.GetComponent<Renderer>();
+
+    // half-sizes of objects
+    Vector3 halfA = anchorRenderer.bounds.extents;   // anchor half-size
+    Vector3 halfB = objectRenderer.bounds.extents;   // object half-size
+
+    // anchor position
+    Vector3 posA = a.transform.position;
+    Vector3 pos = posA;
+
+    // top/bottom of anchor
+    float topA = anchorRenderer.bounds.max.y;
+    float bottomA = anchorRenderer.bounds.min.y;
+
+    // --- sideOne ---
+    switch (sideOne)
+    {
+        case SideOne.right:
+            pos.x = posA.x + (halfA.x - halfB.x) + edgeOneOffset; // keep b inside the right edge
+            pos.y = topA + halfB.y;               // rest on top
+            break;
+
+        case SideOne.left:
+            pos.x = posA.x - (halfA.x - halfB.x) - edgeOneOffset;
+            pos.y = topA + halfB.y;
+            break;
+
+        case SideOne.front:
+            pos.z = posA.z + (halfA.z - halfB.z) + edgeOneOffset;
+            pos.y = topA + halfB.y;
+            break;
+
+        case SideOne.back:
+            pos.z = posA.z - (halfA.z - halfB.z) - edgeOneOffset;
+            pos.y = topA + halfB.y;
+            break;
+
+        case SideOne.above:
+            pos.y = topA + halfB.y + edgeOneOffset;
+            break;
+
+        case SideOne.below:
+            pos.y = bottomA - halfB.y - edgeOneOffset;
+            break;
+    }
+
+    // --- sideTwo ---
+    switch (sideTwo)
+    {
+        case SideTwo.right:
+            pos.x = posA.x + (halfA.x - halfB.x) + edgeTwoOffset; // keep b inside the right edge
+            pos.y = topA + halfB.y;               // rest on top
+            break;
+
+        case SideTwo.left:
+            pos.x = posA.x - (halfA.x - halfB.x) - edgeTwoOffset;
+            pos.y = topA + halfB.y;
+            break;
+
+        case SideTwo.front:
+            pos.z = posA.z + (halfA.z - halfB.z) + edgeTwoOffset;
+            pos.y = topA + halfB.y;
+            break;
+
+        case SideTwo.back:
+            pos.z = posA.z - (halfA.z - halfB.z) - edgeTwoOffset;
+            pos.y = topA + halfB.y;
+            break;
+
+        case SideTwo.above:
+            pos.y = topA + halfB.y + edgeTwoOffset;
+            break;
+
+        case SideTwo.below:
+            pos.y = bottomA - halfB.y - edgeTwoOffset;
+            break;
+    }
+
+    // clamp inside X/Z so b never hangs off the anchor
+    pos.x = Mathf.Clamp(pos.x, posA.x - halfA.x + halfB.x, posA.x + halfA.x - halfB.x);
+    pos.z = Mathf.Clamp(pos.z, posA.z - halfA.z + halfB.z, posA.z + halfA.z - halfB.z);
+
+    // move b into place
+    b.transform.position = pos;
+}
+
+}
